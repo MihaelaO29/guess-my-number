81fe776d5f813b0dc198ed74395b4c854bed6ef7
MihaelaO29 <opriamihaela22@yahoo.com>
2024-04-04 22:03:00 +0300

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import listOfQuiz from './utils';
 
 function App() {
+
+  const [check, setCheck] = useState(false);
+
+  const handleAnswerClicked = (id: number) => {
+    const clickedAnswer = listOfQuiz[0].answers.filter((answer) => id === answer.id)[0]
+    console.log(clickedAnswer)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='tab'>
+      <div className='quiz'>
+        <div className='questionTab'>
+          <p>Question 0/4</p>
+          <br />
+          <p>{listOfQuiz[0].question}</p>
+        </div>
+        <div className='answer'>
+          {listOfQuiz[0].answers.map((answer) => {
+            return (
+              <button onClick={() => handleAnswerClicked(answer.id)} className='allAnswers'>{answer.name}</button>
+            )
+          })}
+
+        </div>
+      </div>
     </div>
-  );
+
+
+  )
 }
 
 export default App;
