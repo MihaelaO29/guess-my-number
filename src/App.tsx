c2db62d0f0279d32c13c6c4d41385fd96ef44c4f
MihaelaO29 <opriamihaela22@yahoo.com>
2024-05-20 14:44:44 +0300

    fixed bugs, added favicon and name

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,88 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
-import listOfQuiz from './utils';
 
 function App() {
+  const [gameNumber, setGameNumber] = useState<number>(0);
+  const [randomNumber, setRandomNumber] = useState(0);
+  const [score, setScore] = useState(20);
+  const [highscore, setHighscore] = useState(0);
+  const [start, setStart] = useState('Start guessing');
+  const [winner, setWinner] = useState('Guess My Number!');
 
-  const [check, setCheck] = useState(false);
+  useEffect(() => {
+    setRandomNumber(generateNewRandomNumber());
+  }, []);
 
-  const handleAnswerClicked = (id: number) => {
-    const clickedAnswer = listOfQuiz[0].answers.filter((answer) => id === answer.id)[0]
-    console.log(clickedAnswer)
-
+  const handleNumber = (event: any) => {
+    setGameNumber(+event.target.value);
   }
-  return (
-    <div className='tab'>
-      <div className='quiz'>
-        <div className='questionTab'>
-          <p>Question 0/4</p>
-          <br />
-          <p>{listOfQuiz[0].question}</p>
-        </div>
-        <div className='answer'>
-          {listOfQuiz[0].answers.map((answer) => {
-            return (
-              <button onClick={() => handleAnswerClicked(answer.id)} className='allAnswers'>{answer.name}</button>
-            )
-          })}
 
+  const generateNewRandomNumber = () => {
+    return Math.floor(Math.random() * 19) + 1;
+  }
+
+  const checkNumber = () => {
+    if (+gameNumber > +randomNumber) {
+      setStart('Too Heigh');
+      setScore(score - 1);
+    } else if (+gameNumber < +randomNumber) {
+      setStart('Too Low');
+      setScore(score - 1);
+    } else if (+gameNumber === +randomNumber) {
+      setHighscore(score);
+      setStart('Perfect');
+      setWinner('Winner');
+      document.body.style.backgroundColor = '#FF0000';
+    }
+  }
+
+  const reset = () => {
+    setRandomNumber(generateNewRandomNumber());
+    setScore(20);
+    document.body.style.backgroundColor = 'rgb(36, 33, 33)';
+  }
+
+  const handleFocus = () => {
+    setGameNumber(0);
+  }
+
+  return (
+    <>
+      <div className='game'>
+        <div className='tab'>
+          <button onClick={reset} className='btn'>Again!</button>
+          <div className='info_game'>(Between 1 and 20)</div>
+        </div>
+        <div className='details'>
+          <div className='titleGame'>{winner}</div>
+          <div className='bar'>
+            <div className='questionMark'>
+              {gameNumber === randomNumber ? randomNumber : '?'}
+              </div>
+          </div>
+        </div>
+        <div className='checkNumberTab'>
+          <div className='checkNumber'>
+            {/* @ts-ignore */}
+            <div className='write_number'>
+              <input type='number' value={gameNumber || ''} onClick={handleFocus}
+               onChange={handleNumber}></input>
+            </div>
+            <div className='checkBtn'>
+              <button onClick={checkNumber} className='btn'>Check!</button>
+            </div>
+          </div>
+          <div className='infoScore'>
+            <p>
+              {start}
+            </p>
+            <p>Score: {score} </p>
+            <p>Highscore: {highscore} </p>
+          </div>
         </div>
       </div>
-    </div>
-
-
+    </>
   )
 }
 
